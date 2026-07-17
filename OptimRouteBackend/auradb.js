f8a833e8c477d396a49ba2fbf9c9d6c3bc96c597
Surajshivam-123 <sj919810@gmail.com>
2026-07-17 21:50:01 +0530

    added database

--- OptimRouteBackend/auradb.js
+++ OptimRouteBackend/auradb.js
@@ -6,6 +6,7 @@ dotenv.config();
 const URI = process.env.NEO4J_URI;
 const USER = process.env.NEO4J_USER;
 const PASSWORD = process.env.NEO4J_PASSWORD;
+const DATABASE = process.env.NEO4J_DATABASE || 'optimroute';
 
 let driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
 export async function runQuery(cypher, params = {}) {
@@ -18,7 +19,7 @@ export async function runQuery(cypher, params = {}) {
   // // Use the driver to run queries
 
   // await driver.close()
-  const session = driver.session({ database: 'optimroute' });
+  const session = driver.session({ database: DATABASE });
   try {
     const result = await session.run(cypher, params);
     return result.records;
